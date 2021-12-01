e701201236ca7c8b9af41aac7c0be5a97ac685b0
The Unespied <83736116+TheUnespied@users.noreply.github.com>
2021-12-01 14:50:14 -0500

    Update Palladium.sol

--- Token/Palladium.sol
+++ Token/Palladium.sol
@@ -137,7 +137,7 @@ contract Palladium is ERC20, Ownable {
     
     function buildWormhole(uint256 _planetID, uint256 _factionId, uint256 _amount) external {
         require(factionWormholes[_planetID][_factionId].created == false, "WORMHOLE ALREADY BUILT");
-        require(_planetID != 1 || _planetID != 2 || _planetID != 3 || _planetID != 0, "CANNOT ATTACK HOME PLANETS");
+        require(_planetID !=0 || _planetID != 1 || _planetID != 2 || _planetID != 3 || _planetID != 4, "CANNOT ATTACK HOME / RESOURCE PLANETS");
         
         burnPalladium(msg.sender, _amount);
         
